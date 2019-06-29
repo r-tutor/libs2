21f549a58d6b5ac45476e98095c07828a05cbf91
Auburngrads <auburngrads@live.com>
2019-06-29 04:10:03 -0400

    Update Packages-3

--- dplyr/tests/testthat/helper-dplyr.R
+++
@@ -1,8 +0,0 @@
-
-# Silence soft-deprecation warnings until next tibble() release
-
-tibble <- function(...) {
-  scoped_options(lifecycle_disable_verbose_retirement = TRUE)
-  tibble::tibble(...)
-}
-data_frame <- tibble
