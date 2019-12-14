87dd17a53f08a060bf535c4ce57f18ba998009ba
Auburngrads <auburngrads@live.com>
2019-12-13 23:02:23 -0500

    Update Packages-3

--- emojifont/doc/emojifont.R
+++ emojifont/doc/emojifont.R
@@ -1,19 +1,19 @@
-## ----style, echo=FALSE, results="asis", message=FALSE--------------------
+## ----style, echo=FALSE, results="asis", message=FALSE-------------------------
 knitr::opts_chunk$set(tidy = FALSE,
 		   message = FALSE)
 
-## ----echo=FALSE, results="hide", message=FALSE---------------------------
+## ----echo=FALSE, results="hide", message=FALSE--------------------------------
 library("ggplot2")
 library("emojifont")
 library("colorspace")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(emojifont)
 
 search_emoji('smile')
 emoji(search_emoji('smile'))
 
-## ----fig.showtext=TRUE---------------------------------------------------
+## ----fig.showtext=TRUE--------------------------------------------------------
 set.seed(123)
 x <- rnorm(10)
 set.seed(321)
@@ -21,7 +21,7 @@ y <- rnorm(10)
 plot(x, y, cex=0)
 text(x, y, labels=emoji('cow'), cex=1.5, col='steelblue', family='EmojiOne')
 
-## ----fig.showtext=TRUE---------------------------------------------------
+## ----fig.showtext=TRUE--------------------------------------------------------
 d <- data.frame(x=x, y=y,
      label = sample(c(emoji('cow'), emoji('camel')), 10, replace=TRUE),
      type = sample(LETTERS[1:3], 10, replace=TRUE))
@@ -30,13 +30,13 @@ library("ggplot2")
 ggplot(d, aes(x, y, color=type, label=label)) +
     geom_text(family="EmojiOne", size=6)
 
-## ----fig.showtext=TRUE---------------------------------------------------
+## ----fig.showtext=TRUE--------------------------------------------------------
 ggplot() + geom_emoji("rose", color='steelblue') + theme_void()
 x = seq(0, 2*pi, length=30)
 y = sin(x)
 ggplot() + geom_emoji('heartbeat', x=x, y=y, size=10)
 
-## ----fig.showtext=TRUE---------------------------------------------------
+## ----fig.showtext=TRUE--------------------------------------------------------
 set.seed(2016-03-09)
 fa <- fontawesome(c('fa-github', 'fa-weibo', 'fa-twitter', 'fa-android', 'fa-coffee'))
 d <- data.frame(x=rnorm(20),
@@ -48,9 +48,9 @@ ggplot(d, aes(x, y, color=label, label=label)) +
     xlab(NULL)+ylab(NULL) +
     theme(legend.text=element_text(family='fontawesome-webfont'))
 
-## ----fig.showtext=TRUE---------------------------------------------------
+## ----fig.showtext=TRUE--------------------------------------------------------
 ggplot() + geom_fontawesome("fa-github", color='black') + theme_void()
 
-## ----echo=FALSE----------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 sessionInfo()
 
